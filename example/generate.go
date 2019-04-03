ff94712d56e0c61bd7f8e85b15b6e84e74f552ed
Gert Drapers <gert.drapers@live.com>
2019-04-02 17:13:08 -0700

    fix namespace example output

--- example/generate.go
+++ example/generate.go
@@ -2,6 +2,6 @@
 
 package test
 
-//go:generate gogen-enum -input ./test.yaml -package main -output ./test.go
+//go:generate gogen-enum -input ./test.yaml -package example -output ./test.go
 //go:generate gofmt -w test.go
 //go:generate golangci-lint run test.go
